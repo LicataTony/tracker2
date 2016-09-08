2552f555b55bfc4474f8c8d23703a9b1fd0d89e3
LicataTony <licatat@studentfr.ch>
2016-09-08 09:16:40 +0200

    fixed publication

--- server/publication.js
+++ server/publication.js
@@ -1,3 +1,13 @@
-Meteor.publish('personnes', function() {
+Meteor.publish('personnesAdmin', function() {
   return Personnes.find();
+});
+
+Meteor.publish('personnesList', function() {
+  var argRequest =    {$and: [
+        {$or: [{hidden: {$exists: false}},{hidden: false}]},
+        {$or: [{blocked: {$exists: false}},{blocked: false}]}
+  ]};
+  var fields  = {fields: {nom: false}};
+  var publication = Personnes.find(argRequest, fields);
+  return publication;
 });
