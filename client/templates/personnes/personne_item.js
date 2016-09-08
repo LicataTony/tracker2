1c943de05a38418f2197794093a559587ff57ae0
LicataTony <licatat@studentfr.ch>
2016-09-08 08:20:20 +0200

    finishing checkbox

--- client/templates/personnes/personne_item.js
+++ client/templates/personnes/personne_item.js
@@ -10,11 +10,35 @@ Template.personneItem.helpers({
   },
   y: function(){
     return this.loc[1];
+  },
+  hidden: function(){
+    if(this.hidden) return 'checked';
+    return '';
+  },
+  blocked: function(){
+    if(this.blocked) return 'checked';
+    return '';
+  },
+  isAdmin: function(){
+    return (Router.current().route.getName().indexOf('personnesAdmin') !== -1);
+  },
+  isHidden: function(){
+    var isAdmin = (Router.current().route.getName().indexOf('personnesAdmin') !==-1);
+    if(isAdmin) return true;
+    if(this.hidden) return false;
+    if(this.blocked) return false;
+    return true;
   }
 });
 
 Template.personneItem.events({
   'click .delete': function(){
     Meteor.call('personneDelete', this._id);
+  },
+  'change .hidden2': function(e){
+    Meteor.call('personneHidden', {id: this._id, hidden: ($(event.target).is(":checked"))});
+  },
+  'change .blocked': function(e){
+    Meteor.call('personneBlocked', {id: this._id, hidden: ($(event.target).is(":checked"))});
   }
 });
---
+++ client/templates/personnes/personnes_admin.js
@@ -0,0 +1,5 @@
+Template.personnesAdmin.helpers({
+  personnes: function(){
+    return Personnes.find();
+  }
+});
